0e4a021c4819973ce71065251524317a2493a777
macean20 <maceanchristopher7@gmail.com>
2026-06-29 10:08:35 +0000

    chore: base application skeleton initialization for Java 17

---
+++ badwallet-api/src/main/java/com/badwallet/BadWalletApplication.java
@@ -0,0 +1,11 @@
+package com.badwallet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BadWalletApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BadWalletApplication.class, args);
+    }
+}
